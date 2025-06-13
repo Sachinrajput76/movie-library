39e673621a2afd5dd5e79e56eade37ca242e595a
Sachin Kumar <sachinrajputlive@gmail.com>
2025-06-13 19:16:17 +0530

    [main] deployment fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
+import { Metadata } from "next";
 import axios from "axios";
 import SearchAndDisplayMovies from "@/components/SearchAndDisplayMovies";
 import { buildSearchUrl } from "@/utils/api";
 import { Film } from "lucide-react";
+import { HomePageProps } from "@/types/movie";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { find?: string };
-}) {
-  const { find } = searchParams || {};
-  const searchValue = find?.trim() || "Avengers";
-
+export default async function Home({ searchParams }: HomePageProps) {
+  const defaultSearchValue = "Avengers";
+  const searchValue = searchParams?.find?.trim() || defaultSearchValue;
 
   let data = null;
 
--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,7 +1,13 @@
-export interface Movie {
+interface Movie {
     imdbID: string;
     Title: string;
     Rating?: string;
     Year: string;
     Poster: string;
 }
+interface HomePageProps {
+    searchParams?: {
+        find?: string;
+    };
+}
+export type { HomePageProps, Movie };
